f0b90cc2c4fd8b46cfb97cb7bb62083b77e23637
Sweatha <sweathadharan1212@gmail.com>
2024-05-08 16:43:40 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
+import React from 'react';
 import './App.css';
+import { imageData} from './components/DataComponent';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
-
 function App() {
-  // code here
+  const imgData = imageData(); 
+  const imageElements = [];
+  for (let i = 0; i < imgData.length; i++) {
+    imageElements.push(
+      <img key={imgData[i].id} src={imgData[i].img} alt={imgData[i].id} />
+    );
+  }
+  return (
+    <div className="App">
+      <h1>Kalvium gallary</h1>
+      <div className="image-container">
+        {imageElements}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,26 @@
-import { Component } from "react";
-import "./App.css"
-import elephant from "./images/elephant.jpeg";
+import React, { Component } from "react";
+import "./App.css";
+import elephant from "./images/elephant.jpeg"; 
+import { imageData} from './components/DataComponent'; 
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
+class AppClass extends Component {
+  render() {
+    const imgData = imageData(); 
+    const imageElements = [];
+    for (let i = 0; i < imgData.length; i++) {
+      imageElements.push(
+        <img key={imgData[i].id} src={imgData[i].img} alt={imgData[i].id} />
+      );
+    }
+    return (
+      <div className="App">
+        <h1>Kalvium gallary</h1>
+        <div className="image-container">
+          {imageElements}
+        </div>
+      </div>
+    );
   }
-
-  // code here
 }
+
+export default AppClass;
